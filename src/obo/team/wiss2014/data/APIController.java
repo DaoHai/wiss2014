183871d5f9467fc17ec16d9c746c58dff65c62f9
Anthony Hombiat <Anthony Hombiat@Shahid-PC>
2014-08-28 23:36:32 +0200

    Fix2

--- src/obo/team/wiss2014/data/APIController.java
+++ src/obo/team/wiss2014/data/APIController.java
@@ -1,5 +1,7 @@
 package obo.team.wiss2014.data;
 
+import java.io.InputStream;
+
 import org.apache.http.HttpResponse;
 
 public class APIController {
@@ -22,6 +24,24 @@ public class APIController {
 	public HttpResponse getForces(String url){
 		HttpResponse response = client.processRequest(url);
 		return response;
+	}
+	
+	public InputStream getCategoriesStream(){
+		String url = "http://data.police.uk/api/crime-categories?date=2011-08";
+		InputStream stream = client.processForStream(url);
+		return stream;
+	}
+	
+	public InputStream getAvailableDatasetStream(){
+		String url = "http://data.police.uk/api/crimes-street-dates";
+		InputStream stream = client.processForStream(url);
+		return stream;
+	}
+	
+	public InputStream getForceStream(){
+		String url = "http://data.police.uk/api/forces";
+		InputStream stream = client.processForStream(url);
+		return stream;
 	}
 
 }
