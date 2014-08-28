f64aafd7e226abe5b279b20e773120fc37fb39a3
'anthonyhombiat' <'anthony.hombiat@imag.fr'>
2014-08-28 21:22:54 +0200

    second comment

--- src/obo/team/wiss2014/Wiss2014UI.java
+++ src/obo/team/wiss2014/Wiss2014UI.java
@@ -1,30 +1,29 @@
 package obo.team.wiss2014;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.File;
 
-import org.vaadin.addon.leaflet.LMap;
-import org.vaadin.addon.leaflet.LOpenStreetMapLayer;
+import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.Reindeer;
-import com.vividsolutions.jts.geom.Coordinate;
 
 @SuppressWarnings("serial")
 @Theme("wiss2014")
 public class Wiss2014UI extends UI {
-
-	public static final Coordinate DEFAULT_CENTER = new Coordinate(45.1841656,5.7155425);
-	public static final int DEFAULT_ZOOM_LEVEL = 12;
-	public static final String OSM_LAYER_NAME = "OpenStreetMap";
-	public static final String OSM_ATTRIBUTION = "Powered by Leaflet with v-leaflet";
 	
-	public static final LMap map = new LMap();
+	private static final Map map = new Map();
+	private static final HoursSlider hoursSlider = new HoursSlider();
+	private static final TypesOfCrimeTable typesOfCrimesTable = new TypesOfCrimeTable();
+	
+	private static final DetailedInfoWindow detailedInfoWindow = new DetailedInfoWindow();
 	
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = Wiss2014UI.class, widgetset = "obo.team.wiss2014.AppWidgetSet")
@@ -34,37 +33,70 @@ public class Wiss2014UI extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 
-		Label h1 = new Label("CrimeWatch");
-		h1.setStyleName(Reindeer.LABEL_H1);
+		Label title = new Label("WISS2014 - iCrimeRates");
+		title.setStyleName(Reindeer.LABEL_H1);
 		
-		Label h2 = new Label("WISS2014");
-		h2.setStyleName(Reindeer.LABEL_H2);
+		Label subtitle = new Label("by OBO team");
+		subtitle.setStyleName(Reindeer.LABEL_SMALL);
 		
-		Label h3 = new Label("OBO team");
-		h3.setStyleName(Reindeer.LABEL_SMALL);
+		final VerticalLayout mapAndSliderLayout = new VerticalLayout();
 		
-		map.setAttributionPrefix(OSM_ATTRIBUTION);
-		map.addBaseLayer(new LOpenStreetMapLayer(), OSM_LAYER_NAME);
-		map.setZoomLevel(DEFAULT_ZOOM_LEVEL);
-		map.setCenter(DEFAULT_CENTER.x, DEFAULT_CENTER.y);
-		map.setSizeFull();
+		mapAndSliderLayout.addComponent(map);
+		mapAndSliderLayout.setExpandRatio(map, 0.9f);
+		mapAndSliderLayout.addComponent(hoursSlider);
+		mapAndSliderLayout.setExpandRatio(hoursSlider, 0.1f);
+		mapAndSliderLayout.setComponentAlignment(hoursSlider, Alignment.MIDDLE_CENTER);
+		
+		mapAndSliderLayout.setSizeFull();
+		
+		final HorizontalLayout horizontalLayout = new HorizontalLayout();
+		
+		horizontalLayout.addComponent(typesOfCrimesTable);
+		horizontalLayout.setExpandRatio(typesOfCrimesTable, 0.25f);
+		horizontalLayout.addComponent(mapAndSliderLayout);
+		horizontalLayout.setExpandRatio(mapAndSliderLayout, 0.75f);
+		
+		horizontalLayout.setSpacing(true);
+		horizontalLayout.setSizeFull();
 		
 		final VerticalLayout layout = new VerticalLayout();
 		
-		layout.addComponent(h1);
-		layout.setExpandRatio(h1, 0.05f);
-		layout.addComponent(h2);
-		layout.setExpandRatio(h2, 0.05f);
-		layout.addComponent(h3);
-		layout.setExpandRatio(h3, 0.05f);
-		layout.addComponent(map);
-		layout.setExpandRatio(map, 0.85f);
+		layout.addComponent(title);
+		layout.setExpandRatio(title, 0.05f);
+		layout.addComponent(subtitle);
+		layout.setExpandRatio(subtitle, 0.05f);
+		layout.addComponent(horizontalLayout);
+		layout.setExpandRatio(horizontalLayout, 0.9f);
 		
 		layout.setMargin(true);
+		layout.setSpacing(true);
 		layout.setSizeFull();
 		
 		this.setContent(layout);
 		this.setSizeFull();
+		
+		new Controller(this);
+	}
+	
+	public void populateMap(){
+		RdfReader reader = new RdfReader(new File("D:\\anthony_docs\\workspace_kepler\\WISS2014\\WebContent\\WEB-INF\\resources\\2013-06-btp-street-csv.rdf"));
+		reader.read();
+	}
+	
+	public Map getMap(){
+		return map;
+	}
+	
+	public TypesOfCrimeTable getTypesOfCrimesTable(){
+		return typesOfCrimesTable;
+	}
+	
+	public HoursSlider getHoursSlider(){
+		return hoursSlider;
 	}
 
+	public DetailedInfoWindow getDetailedInfoWindow(){
+		return detailedInfoWindow;
+	}
+	
 }
